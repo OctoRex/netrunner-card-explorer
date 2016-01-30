6f95eee1c178c7cb16ddffd8e5ff2a523b0ab2cb
Iain Kydd <iain.kydd@gmail.com>
2016-01-30 13:03:50 +0000

    Fixed subtype overlap

--- client/filters/suptypeOptions.js
+++ client/filters/suptypeOptions.js
@@ -1,7 +1,7 @@
 app.filter('subtypeOptions', function(){
-  return function (filters, types) {
+  return function (filters, types, side) {
     return filters.filter(function(filter){
-      return filter.types.filter(function(type) {
+      return filter.types[side].filter(function(type) {
         return types.indexOf(type) != -1;
       }).length;
     });
--- server/data-parser.js
+++ server/data-parser.js
@@ -68,30 +68,52 @@ module.exports = {
       corp: ['none'],
       runner: ['none']
     };
-    var subtypes = { 'none': {value: 'none', label: 'No subtype', side: 'corprunner', types:[]}};
+    // start our array off with none as that will never be picked up
+    var subtypes = { 'none': {value: 'none', label: 'No subtype', side: 'corprunner', types: {'corp':[], 'runner':[]}}};
     
     cards.forEach(function(card){
+      // get the side of the card, we'll need it later
       var side = card.side_code;
+      // if the card has subtypes ...
       if (card.subtype_code) {
+        // get an array of the codes
         var codes = card.subtype_code.split(' - ');
+        // get an array of the display names
         var names = card.subtype.split(' - ');
+        // handle each code separately
         codes.forEach(function(code, index) {
+          // get the display name from the other array
           var name = names[index];
+          // if this subtype is not defined in our list yet, we need to initialise it
+          // and populate it with the data for this code
           if (typeof subtypes[code] == 'undefined') {
-            subtypes[code] = {value: code, label: name, side: side, types: [card.type_code]};
+            // note that the types apply to each side separately
+            // otherwise it looks weird on the filter list
+            var subtype = {value: code, label: name, side: side, types: {corp:[], runner:[]}};
+            subtype.types[side].push(card.type_code);
+            // after we've created the subtype, add it to the list, keying by the codename
+            subtypes[code] = subtype;
+            // add it to our list that keeps a track of whether we added the text for that
+            // side or not
+            subtypeCodes[side].push(code);
           } else {
+            // if we have the code already in the list then we need to check a couple things
+            // 1. is this from a different side from before, if so add it the subtype
             if (subtypeCodes[side].indexOf(code) == -1) {
               subtypeCodes[side].push(code);
               subtypes[code].side += side;
             }
-            if (subtypes[code].types.indexOf(card.type_code) == -1) {
-              subtypes[code].types.push(card.type_code);
+            // 2. is this a card that has new types that apply to the subtype
+            if (subtypes[code].types[side].indexOf(card.type_code) == -1) {
+              subtypes[code].types[side].push(card.type_code);
             }
           }
         });
       } else {
-        if (subtypes['none'].types.indexOf(card.type_code) == -1) {
-          subtypes['none'].types.push(card.type_code);
+        // ... but if it doesn't then we need to add it to the 'none' subtype
+        // note that 'none' is already in the subtypes-per-side list so we don't have to add it
+        if (subtypes['none'].types[side].indexOf(card.type_code) == -1) {
+          subtypes['none'].types[side].push(card.type_code);
         }
       }
     });
