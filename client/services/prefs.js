faa097ec5dada956ba781ddee141c423b539cfd5
Iain Kydd <iain.kydd@gmail.com>
2016-02-07 16:02:46 +0000

    removed borked WIP service filter

--- client/services/prefs.js
+++ client/services/prefs.js
@@ -1,25 +1,25 @@
-app.service('PrefsSvc', function(HelperSvc){
+// app.service('PrefsSvc', function(HelperSvc){
   
-  this.helper = HelperSvc;
+  // this.helper = HelperSvc;
   
-  this.prefs = [
-    {
-      title: 'Show spoilers',
-      value: 
-    }
-  ]
+  // this.prefs = [
+    // {
+      // title: 'Show spoilers',
+      // value: 
+    // }
+  // ]
   
-  this.setSpecials = function() {
-    this.specials.selected = [];
-    this.specials.all.forEach(function(special){
-      if (special.selected) {
-        this.specials.selected.push(special.value);
-      }
-    }, this);
-  }
+  // this.setSpecials = function() {
+    // this.specials.selected = [];
+    // this.specials.all.forEach(function(special){
+      // if (special.selected) {
+        // this.specials.selected.push(special.value);
+      // }
+    // }, this);
+  // }
   
-  this.noSpecials = function() {
-    this.specials.all.forEach(this.helper.checkAll(false));
-    this.setSpecials();
-  }
-});
+  // this.noSpecials = function() {
+    // this.specials.all.forEach(this.helper.checkAll(false));
+    // this.setSpecials();
+  // }
+// });
