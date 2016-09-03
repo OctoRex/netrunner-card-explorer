5bc58d70e32618df1608016b34e706cd1e71d800
Iain Kydd <iain.kydd@gmail.com>
2016-09-03 01:11:59 +0100

    bit of re-organising the files

---
+++ server/modules/responses.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  ok : function(res, content) {
+    res.status(200).type('json').json(content);
+  },
+
+  notModified: function(res, result) {
+    res.status(304).send();
+  },
+  
+  error : function(res, reason) {
+    console.log(reason);
+    res.status(500).type('json').send({"message": "Problem with the server"});
+  }
+}
