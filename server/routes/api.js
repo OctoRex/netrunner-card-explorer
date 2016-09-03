6afc7de9366d035ba9ac4dd6121ef07527b3b51a
Iain Kydd <iain.kydd@gmail.com>
2016-09-03 01:12:45 +0100

    missed out a file or two

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,16 @@
 var express = require('express')
   , router = express.Router()
-  , db = require('../modules/db');
-
+  , db = require('../modules/db')
+  , response = require('../modules/responses')
+  
+/**
+ *  Just return the modified time from the request to kick
+ *  off our promises
+ */
 function getModifiedTime(req) {
   return new Promise((resolve, reject) => {
     if (req.get('If-Modified-Since')) {
+      // change it to unix timestamp
       resolve(new Date(req.get('If-Modified-Since')).getTime());
     } else {
       reject('Requires new data');
@@ -12,12 +18,21 @@ function getModifiedTime(req) {
   });
 }
 
-function compareToModifiedtime(cacheTime, collcetion) {
+/**
+ *  Get the last modified time from the DB and compare to that
+ *  provided in the request (if there isn't one it will have be
+ *  caught elsewhere
+ */
+function compareToModifiedtime(cacheTime, collection) {
   return new Promise((resolve, reject) => {
-    db.collection('modified').findOne({'collection': collcetion}, (err, doc) => {
+    // pull out the record from the DB
+    db.collection('modified').findOne({'collection': collection}, (err, doc) => {
       if (err) reject('No modified time');
       resolve(doc.modified);
     });
+  // compare the two times, we have to do this in here because
+  // we need both variables and adding this 'then' outside
+  // makes that a bit tricky
   }).then(function(modified) {
     if (modified > cacheTime) {
       throw 'Cached copy out of date';
@@ -26,43 +41,39 @@ function compareToModifiedtime(cacheTime, collcetion) {
   });;
 }
 
+/**
+ *  name the collection name you want and this will get it
+ *  for you, simple, just wrapping it in a promise
+ */
 function getData(collectionName) {
   return new Promise((resolve, reject) => {
     db.collection(collectionName).find((err, docs) => {
       if (err) reject(err);
       resolve(docs);
-    });
+    })
   });
 }
 
-function error(res, reason) {
-  res.status(500).send('Problem with the server');
-}
-
-function ok(res, docs) {
-  res.status(200).type('json').json(docs);
-}
-
-function notModified(res, result) {
-  res.status(304).send();
-}
-
+/**
+ *  A wrapper for adding it to the router as they're all the same really
+ */
 function apiCall(router, endpoint, collection) {
   router.get(endpoint, function(req, res) {
+       
     getModifiedTime(req).then(result => { 
       return compareToModifiedtime(result, collection);
     // if no new results then show the cache
     }).then(result => {
-      notModified(res, result);
+      responses.notModified(res, result);
     // if that fails or some reason, or the data is new then
     // show the data
     }).catch(reason => { 
       return getData(collection).then(docs => {
-        ok(res, docs);
-      }).catch(reason => {
-        error(res, reason);
-      });
-    })
+        responses.ok(res, docs);
+      })
+    }).catch(reason => {
+      responses.error(res, reason);
+    });
   });
 }
 
